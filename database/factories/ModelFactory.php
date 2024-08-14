a588de433536c6120486b6871f83901b19ca48f3
dangnhdev <dangnhdev@users.noreply.github.com>
2024-08-14 11:23:57 +0000

    Fix styling

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Hdecom\EtsySdk\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Hdecom\EtsySdk\Tests;
 
 use Hdecom\EtsySdk\EtsySdkServiceProvider;
